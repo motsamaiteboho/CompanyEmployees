6703dcaebbc336b625b29993b1d158828f025a9e
motsamaiteboho <96079592+motsamaiteboho@users.noreply.github.com>
2022-08-01 11:39:08 +0200

    added searching

--- Shared/RequestFeatures/EmployeeParameters.cs
+++ Shared/RequestFeatures/EmployeeParameters.cs
@@ -11,5 +11,6 @@ namespace Shared.RequestFeatures
         public uint MinAge { get; set; }
         public uint MaxAge { get; set; } = int.MaxValue; 
         public bool ValidAgeRange => MaxAge > MinAge;
+        public string? SearchTerm { get; set; }
     }
 }
